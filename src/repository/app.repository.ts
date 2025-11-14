b2ef26e7c1737dda6534f3a3d097273ad7d3fb62
pavlovadb <pavlovadb@gmail.com>
2025-11-13 16:22:12 -0800

    added get route for all documents, able to filter by status

--- src/repository/app.repository.ts
+++ src/repository/app.repository.ts
@@ -3,6 +3,8 @@ import {
   DynamoDBDocumentClient,
   GetCommand,
   PutCommand,
+  QueryCommand,
+  ScanCommand,
   UpdateCommand,
 } from '@aws-sdk/lib-dynamodb';
 import { DocumentData, DocumentStatus } from '../service/types';
@@ -23,7 +25,8 @@ export class AppRepository {
   }
 
   async createDocument(
-    documentData: Omit<DocumentData, 'documentId' | 'status'>, documentId: string
+    documentData: Omit<DocumentData, 'documentId' | 'status'>,
+    documentId: string
   ): Promise<string> {
     const command = new PutCommand({
       TableName: this.tableName,
@@ -85,5 +88,57 @@ export class AppRepository {
     }
 
     return response.Attributes as DocumentData;
+  }
+
+  async fetchDocumentsByStatus(
+    page: number,
+    limit: number,
+    status: string
+  ): Promise<DocumentData[]> {
+    return [];
+  }
+
+  async fetchAllDocuments(
+    page: number,
+    limit: number,
+    status: string
+  ): Promise<DocumentData[]> {
+    try {
+      if (status === 'all') {
+        const command = new ScanCommand({
+          TableName: this.tableName,
+        });
+
+        const response = await this.docClient.send(command);
+
+        if (response.$metadata.httpStatusCode !== 200) {
+          throw new Error(`Failed to fetch documents`);
+        }
+
+        return (response.Items as DocumentData[]) || [];
+      }
+
+      const command = new ScanCommand({
+        TableName: this.tableName,
+        FilterExpression: '#status = :status',
+        ExpressionAttributeNames: {
+          '#status': 'status',
+        },
+        ExpressionAttributeValues: {
+          ':status': status,
+        },
+      });
+
+      const response = await this.docClient.send(command);
+
+      if (response.$metadata.httpStatusCode !== 200) {
+        throw new Error(`Failed to fetch documents with status: ${status}`);
+      }
+
+      return (response.Items as DocumentData[]) || [];
+    } catch (error) {
+      console.error('Error fetching documents:', error);
+      throw new Error('Failed to fetch documents from database');
+    }
   }
 }
